4c88ba0d79cce3a3fcc9daf78afc4bff98a8d665
Kim-hanseul <hansul80@gmail.com>
2022-03-28 16:53:47 +0900

    2022.03.28

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-import React,{useState} from "react"
 export default function Home() {
-    const [count,setCount] = useState(0);
-    return (
-    <>
-    <button onClick={()=>{setCount(count+1)}}>+</button> <br/>
-    <button onClick={()=>{setCount(count-1)}}>-</button>
-    <div>{count}</div>
+  return (<>
+    <h1>HOME</h1>
     </>
   )
 }
